25c5f1b3aa257c99eb9bfd652d1ef5c41b83e8dc
Daniel Manrique <roadmr@ubuntu.com>
2016-09-07 14:41:33 -0400

    Move sshtron HTTP mini server to port 3080 - avoid clashes with other daemons which use port 3000

--- sshtron.sh
+++ sshtron.sh
@@ -3,5 +3,7 @@
 cd "$SNAP_DATA"
 
 [ -f id_rsa ] || $SNAP/usr/bin/ssh-keygen -t rsa -N '' -f id_rsa
-
+# Define port 3080 so it doesn't clash with other users of port 300 (e.g.
+# Rocket.Chat)
+export PORT=3080
 sshtron > "$SNAP_DATA/sshtron.log" 2>&1
